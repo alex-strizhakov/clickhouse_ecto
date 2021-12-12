f76179af230dfeea0616f5d1a0c13c7a14d5e986
Alexander Strizhakov <alex.strizhakov@gmail.com>
2021-12-12 15:38:11 +0300

    removing examples

--- examples/priv/repo/migrations/20180404182017_add_user_agent_to_clicks.exs
+++
@@ -1,9 +0,0 @@
-defmodule ExampleApp.Repo.Migrations.AddUserAgentToClicks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:clicks) do
-      add :user_agent, :string
-    end
-  end
-end
--- examples/web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule ExampleApp.PageController do
-  use ExampleApp.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
